51fc8aac0c2dbe6d3f3095985b34bbf85b14ecaa
yangpan <916780135@qq.com>
2019-06-06 17:25:40 +0800

    Hystrix 断路器/熔断器

---
+++ yangpan-hystrix-feign/src/main/java/site/yangpan/hystrix/feign/ScheduleFeignClientHystrix.java
@@ -0,0 +1,15 @@
+package site.yangpan.hystrix.feign;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * ScheduleFeignClientHystrix 实现 ScheduleFeignClient接口，它实现的具体方法就是熔断的方法
+ * Created by yangpan on 2019-06-06 15:50.
+ */
+@Component
+public class ScheduleFeignClientHystrix implements ScheduleFeignClient {
+    @Override
+    public String callEurekaClientService() {
+        return "通过Feign调用服务出现错误！！Hystrix进行了熔断。";
+    }
+}
---
+++ yangpan-hystrix-ribbon/src/main/java/site/yangpan/hystrix/controller/HystrixRibbonController.java
@@ -0,0 +1,34 @@
+package site.yangpan.hystrix.controller;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * HystrixRibbonController
+ * Created by yangpan on 2019-06-06 15:12.
+ */
+@RestController
+@RequestMapping("/hystrixRibbon")
+public class HystrixRibbonController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    /**
+     * @HystrixCommand注解标明了fallbackMethod熔断方法
+     */
+    @RequestMapping("/call")
+    @HystrixCommand(fallbackMethod = "callError")
+    public String call(){
+        String result = restTemplate.getForObject("http://yangpan-eureka-client/test", String.class);
+        return "通过Ribbon + RestTemplate调用 => " + result;
+    }
+
+    public String callError(){
+        return "通过Ribbon + RestTemplate调用服务出现错误！！Hystrix进行了熔断。";
+    }
+
+}
