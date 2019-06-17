362516ffe1f1f11b96aacae805cb638d54e408ee
yangpan <916780135@qq.com>
2019-06-17 17:26:58 +0800

    Hystrix Dashboard 断路器/熔断器监控

---
+++ yangpan-hystrix-dashboard-feign/src/main/java/site/yangpan/hystrix/dashboard/HystrixDashboardFeignApplication.java
@@ -0,0 +1,34 @@
+package site.yangpan.hystrix.dashboard;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableFeignClients
+@EnableHystrixDashboard
+public class HystrixDashboardFeignApplication {
+
+    @Bean
+    public ServletRegistrationBean getServlet(){
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardFeignApplication.class, args);
+    }
+
+}
