1e8dc046b0d9f9d9c45c640c4a08dbc0007a6c78
yangpan <916780135@qq.com>
2019-06-18 16:52:05 +0800

    Gateway 网关初识

---
+++ yangpan-spring-cloud-gateway-helloworld/src/main/java/site/yangpan/gateway/GatewayHelloworldApplication.java
@@ -0,0 +1,47 @@
+package site.yangpan.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@SpringBootApplication
+@RestController
+public class GatewayHelloworldApplication {
+
+
+    @Bean
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+        String httpUri = "http://httpbin.org:80";
+        return builder.routes()
+                .route(p -> p
+                        .path("/get")
+                        .filters(f -> f.addRequestHeader("Hello", "World"))
+                        .uri(httpUri))
+                .route(p -> p
+                        .host("*.hystrix.com")
+                        .filters(f -> f
+                                .hystrix(config -> config
+                                        .setName("mycmd")
+                                        .setFallbackUri("forward:/fallback")))
+                        .uri(httpUri))
+                .build();
+    }
+
+
+    @RequestMapping("/fallback")
+    public Mono<String> fallback() {
+        return Mono.just("Gateway 使用 Hystrix进行熔断操作。。");
+    }
+
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayHelloworldApplication.class, args);
+    }
+
+}
