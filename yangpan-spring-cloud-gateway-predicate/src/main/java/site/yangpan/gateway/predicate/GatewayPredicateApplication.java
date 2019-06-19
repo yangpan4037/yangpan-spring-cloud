9442be2f4254a5cf9fb4fe4eadd7c93d7ff9addb
yangpan <916780135@qq.com>
2019-06-20 00:07:58 +0800

    Gateway Predicate 路由谓词

---
+++ yangpan-spring-cloud-gateway-predicate/src/main/java/site/yangpan/gateway/predicate/GatewayPredicateApplication.java
@@ -0,0 +1,13 @@
+package site.yangpan.gateway.predicate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayPredicateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayPredicateApplication.class, args);
+    }
+
+}
---
+++ yangpan-spring-cloud-gateway-predicate/src/test/java/site/yangpan/gateway/predicate/GatewayPredicateApplicationTests.java
@@ -0,0 +1,16 @@
+package site.yangpan.gateway.predicate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GatewayPredicateApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
